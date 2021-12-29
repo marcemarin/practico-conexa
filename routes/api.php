a29f7f78274b00511d95e90e4c9e3635299a15d8
marcemarin-kw <mmarin@kubikware.com>
2021-12-29 14:47:57 -0300

    names fix

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/earth', [EarthController::class, 'index']);
+Route::get('/earth', [EarthController::class, 'earth']);
 
-Route::post('/earth', [EarthController::class, 'store']);
+Route::post('/final-position', [EarthController::class, 'finalPosition']);
